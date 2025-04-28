98c01d7d39d9506484906374d3a20cf21b6cfeb7
Jyotirmoy Chakraborty <jyotirmoychakraborty35@gmail.com>
2025-04-29 01:59:53 +0530

    Create Model Initialization.py
    Creates generator and discriminator instances
    
    Moves models to appropriate device (GPU/CPU)
    
    Defines Binary Cross Entropy loss
    
    Sets up Adam optimizers for both networks

---
+++ Model Initialization.py
@@ -0,0 +1,6 @@
+generator = Generator().to(device)
+discriminator = Discriminator().to(device)
+
+adversarial_loss = nn.BCELoss()
+optimizer_G = optim.Adam(generator.parameters(), lr=learning_rate, betas=(0.5, 0.999))
+optimizer_D = optim.Adam(discriminator.parameters(), lr=learning_rate, betas=(0.5, 0.999))
